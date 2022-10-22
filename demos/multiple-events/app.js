7d40f167461d21e3b43305ad41765493c8194579
ryang223 <ryangoodwd@gmail.com>
2022-10-22 13:47:00 -0400

    ability to enter friend name; addfirend, list

--- demos/multiple-events/app.js
+++ demos/multiple-events/app.js
@@ -1,7 +1,21 @@
 const removeButtons = document.querySelectorAll('li button');
+const form = document.querySelector('#add-friend');
+const input = document.querySelector('#first-name');
+const friendList = document.querySelector('#friend-list');
 
 for(let btn of removeButtons){
-    btn.addEventListener('click', function(){
-        console.log('YOU CLICKED REMOVE!');
+    btn.addEventListener('click', function(e){
+        e.target.parentElement.remove();
     });
 }
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(input.value)
+    const newFriend = document.createElement('li');
+    newFriend.innerText = input.value;
+    input.value = '';
+    friendList.appendChild(newFriend);
+})
+
