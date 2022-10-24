7a42d2834af063833206efd21ef6a2a2d0186e32
ryang223 <ryangoodwd@gmail.com>
2022-10-24 09:08:31 -0400

    logic delegation of events condensed

--- demos/multiple-events/app.js
+++ demos/multiple-events/app.js
@@ -1,24 +1,33 @@
-const removeButtons = document.querySelectorAll('li button');
+// const form = document.querySelector('#add-friend');
+// const input = document.querySelector('#first-name');
+// const friendList = document.querySelector('#friend-list');
+
+// for(let btn of removeButtons){
+//     btn.addEventListener('click', function(e){
+//         e.target.parentElement.remove();
+//     });
+// }
+
 const form = document.querySelector('#add-friend');
 const input = document.querySelector('#first-name');
-const friendList = document.querySelector('#friend-list');
+const friendList = document.querySelector('#friend-list')
 
-for(let btn of removeButtons){
-    btn.addEventListener('click', function(e){
+
+friendList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON') {
         e.target.parentElement.remove();
-    });
-}
+    }
+});
 
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(input.value)
     const newFriend = document.createElement('li');
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'UnFriend';
-    removeBtn.addEventListener('click', function(e){
-        e.target.parentElement.remove();
-    });
+
+   
     newFriend.innerText = input.value;
     newFriend.appendChild(removeBtn);
     friendList.appendChild(newFriend);
